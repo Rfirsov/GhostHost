bdc0ac1b206e2f400d59224aac9926cb617cc0dd
rfirsov <froma2015@gmail.com>
2017-04-10 11:12:28 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$('.my-slider').unslider({ 
+		autoplay: true, 
+		keys: false,
+		arrows: false,
+		speed: 1000,
+		infinite: true,
+		delay: 6000 
+	});
+});
